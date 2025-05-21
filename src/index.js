c91e2f0654649abd110b03ba3c3263699f470005
Super-bak <yacineiabnidas@gmail.com>
2025-05-21 19:21:08 +0000

    s

--- src/index.js
+++ src/index.js
@@ -21,7 +21,11 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'https://web-dev-client-final-b5xaazyqt-alks-projects-f46b8ef3.vercel.app',
+  credentials: true // optional: only if using cookies or authorization headers
+}));
+
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
